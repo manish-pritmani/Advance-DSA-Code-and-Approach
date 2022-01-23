37ddabf5595093bbc7dadcdf760828edbd09ff55
Manish Pritmani <65852995+manish-pritmani@users.noreply.github.com>
2022-01-23 18:51:28 +0000

    5

--- Tree/PrateekTree.cpp
+++ Tree/PrateekTree.cpp
@@ -113,6 +113,7 @@ void bfs(node* root){
 }
 
 // Approach 1 - BFS using queue - Level order printing
+// pair of node with it's level.
 void bfsApproach1(node* root){
   queue<pair<node*,int>> q;
   q.push(make_pair(root,1));
@@ -131,6 +132,85 @@ void bfsApproach1(node* root){
       q.push(make_pair(front->right,currentLevel+1));
     }
   }
+}
+// BFS approach level order traversal with NULL conditions
+void bfsApp2(node* root){
+  queue<node*> q;
+  q.push(root);
+  q.push(NULL);
+  while(!q.empty()){
+    if(q.front()== NULL){
+      cout<<endl;
+      q.pop();
+      if(!q.empty()){
+        q.push(NULL);
+      }
+      continue;
+    }
+    node* front = q.front();
+    cout<<front->data<<" ";
+    q.pop();
+    if(front->left){
+      q.push(front->left);
+    }
+    if(front->right){
+      q.push(front->right);
+    }
+  }
+}
+
+// countNodes complexity - O(N) as visiting every node
+int countNodes(node* root){
+  if(root == nullptr){
+    return 0;
+  }
+  int leftCount = countNodes(root->left);
+  int rightCount = countNodes(root->right);
+  return 1+leftCount+rightCount;
+}
+
+int sumAllNode(node* root){
+  if(root == nullptr){
+    return 0;
+  }
+  return root->data + sumAllNode(root->left)+sumAllNode(root->right);
+}
+
+int sumInAll(node* root,int sum){
+  if(root==nullptr){
+    sum = 0;
+    return sum;
+  }
+  sum+=root->data+sumInAll(root->left,root->data)+sumInAll(root->right,root->data);
+}
+
+// program to find diameter of tree.
+// Time complexity - O(n*n)
+int diameterTree(node* root){
+  if(root == nullptr){
+    return 0;
+  }
+  int h1 = height(root->left);
+  int h2 = height(root->right);
+  int dia = h1+h2;
+  return max(dia,max(diameterTree(root->left),diameterTree(root->right)));
+}
+
+// Diameter of tree in O(n)
+pair<int,int> diameterTreeBetter(node* root){
+  pair<int,int> p;
+  // first -> height
+  // second -> diameter
+  if(root == nullptr){
+    p.first = 0;
+    p.second = 0;
+    return p;
+  }
+  pair<int,int> left = diameterTreeBetter(root->left);
+  pair<int,int> right = diameterTreeBetter(root->right);
+  p.first = max(left.first,right.first)+1;
+  p.second = max(left.first+right.first,max(left.second,right.second));
+  return p;
 }
 
 int main(){
@@ -157,12 +237,14 @@ int main(){
   cout<<"Level Order";
   levelOrder(root,hTree);
   cout<<endl;
-  cout<<"BFS : ";
-  bfsApproach1(root);
-
-
-
-
-
+  cout<<"BFS : "<<endl;
+  bfsApp2(root);
+  cout<<endl;
+  cout<<"Number of nodes : "<<countNodes(root)<<endl;
+  cout<<"Sum of all nodes : "<<sumAllNode(root)<<endl;
+  cout<<"Sum of all nodes : "<<sumInAll(root,0)<<endl;
+  cout<<"Diameter of Tree : "<<diameterTree(root)<<endl;
+  cout<<"Height Better : "<<diameterTreeBetter(root).first<<endl;
+  cout<<"Diameter Better : "<<diameterTreeBetter(root).second<<endl;
   return 0;
 }
